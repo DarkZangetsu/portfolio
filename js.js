a148e785e09d141c33411dd92b31be2194a01e6a
DarkZangetsu <113225391+DarkZangetsu@users.noreply.github.com>
2023-04-27 14:48:48 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right' });
 //  typed js 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Developpeur Backend', 'Designer', 'Lead dev'],
+    strings: ['Backend Developper', 'Designer', 'Lead dev'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 100,
